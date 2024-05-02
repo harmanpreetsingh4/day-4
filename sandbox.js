5600e88cb3880d643c2d4d53f8be3100b3300620
Harmanpreet Singh <harmanpreetsingh4@academic.rrc.ca>
2024-05-02 11:28:08 -0500

    Worked on strings

--- sandbox.js
+++ sandbox.js
@@ -5,6 +5,7 @@ console.log(2);
 
 let age = 25;
 let year = 2019;
+// log things to console
 console.log(age, year);
 
 age = 30
@@ -15,3 +16,11 @@ console.log(points);
 
 var score = 75;
 console.log(score);
+
+
+// strings
+console.log('hello, world');
+
+let email = 'email@example.com';
+console.log(email);
+
