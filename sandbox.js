72078bbcd3a193b7532496a7aa4562dad4b903a0
Harmanpreet Singh <harmanpreetsingh4@academic.rrc.ca>
2024-05-02 14:38:22 -0500

    Worked on numbers and operators

--- sandbox.js
+++ sandbox.js
@@ -40,16 +40,48 @@ console.log(fullName.length);
 // string methods
 console.log(fullName.toUpperCase());
 
-let result = fullName.toLowerCase();
-console.log (result, fullName);
+// let result = fullName.toLowerCase();
+// console.log (result, fullName);
 
 let index = email.indexOf('@');
 console.log(index);
 
 // common string methods
 let email1 = 'email@example.com';
-// let result1 = email.lastIndexOf('e');
-// let result1 = email.slice(4, 10); # start from 4 and end at 10
-let result1 = email.substr(4, 10); // # start from 4 and outputs 10 characters in total
+// let result = email.lastIndexOf('e');
+// let result = email.slice(4, 10); # start from 4 and end at 10
+// let result = email.substr(4, 10); # start from 4 and outputs 10 characters in total
+// let result = email.replace('e', 'h'); // #Only replaces the first e not all e's
 
-console.log(result1);
+// console.log(result);
+
+// Numbers
+let radius = 10;
+const pi = 3.14;
+
+// console.log(radius, pi);
+// math operators +, -, *, /, **, %
+
+// console.log(10 / 2);
+
+// let result = radius % 3;
+// let result = pi * radius**2;
+
+// order of operation - B I D M A S
+
+// let result = 5 * (10-3)**2;
+
+// console.log(result);
+
+let likes = 10;
+// likes = likes + 1;
+// likes++;
+// likes--;
+// likes += 10;
+// likes -= 5;
+// likes *= 2;
+// likes /= 2;
+
+// console.log(likes);
+
+// NaN - not a number
