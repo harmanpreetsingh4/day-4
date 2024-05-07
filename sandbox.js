9b16ec66af43d790930e272cec6613d2e7a53f59
Harmanpreet Singh <harmanpreetsingh4@academic.rrc.ca>
2024-05-07 14:33:09 -0500

    Worked on Variables & Block Scope

--- sandbox.js
+++ sandbox.js
@@ -312,25 +312,40 @@ const pi = 3.14;
 // }
 
 // switch statements
-const grade = '50';
+// const grade = '50';
 
-switch(grade){
-    case '50':
-        console.log('you got an A!');
-        break;
-    case 'B':
-        console.log('you got a B!');
-        break;
-    case 'C':
-        console.log('you got a C!');
-        break;
-    case 'D':
-        console.log('you got a D!');
-        break;
-    case 'E':
-        console.log('you got an E!');
-        break;
-    default:
-        console.log('not a valid grade');
+// switch(grade){
+//     case '50':
+//         console.log('you got an A!');
+//         break;
+//     case 'B':
+//         console.log('you got a B!');
+//         break;
+//     case 'C':
+//         console.log('you got a C!');
+//         break;
+//     case 'D':
+//         console.log('you got a D!');
+//         break;
+//     case 'E':
+//         console.log('you got an E!');
+//         break;
+//     default:
+//         console.log('not a valid grade');
+// }
+
+// variables & block scope 
+const age = 30;
+
+if(true){
+    const age = 40;
+    const name = 'hrmn'
+    console.log('inside 1st code block:', age, name);
+
+    if(true){
+        const age = 50;
+        console.log('inside 2nd code block:', age);
+    }
 }
 
+console.log('outside code block:', age, name);
