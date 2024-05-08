a09e016c1d53a6ac56476f0fe845ee735359762b
Harmanpreet Singh <harmanpreetsingh4@academic.rrc.ca>
2024-05-08 08:36:19 -0500

    Practicing Arrow Functions

--- sandbox.js
+++ sandbox.js
@@ -393,3 +393,31 @@
 
 // const area = calcArea(5);
 // console.log('area is:', area);
+
+// practice arrow functions 
+
+// const greet = function(){
+//     return 'hello, world';
+// };
+
+// const greet = () => 'hello, world';
+// const result = greet ();
+// console.log(result);
+
+// const bill = function(products, tax){
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// };
+
+// const bill = (products, tax) => {
+//     let total = 0;
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// };
+
+// console.log(bill([10,15,30], 0.2));
