05cee9fe608a53de40af36ec0a1b0eeab49f661a
Harmanpreet Singh <harmanpreetsingh4@academic.rrc.ca>
2024-05-08 13:37:45 -0500

    Get elements by their ID, Class Name, Tag Name

--- sandbox.js
+++ sandbox.js
@@ -595,4 +595,16 @@
 // console.log(paras[2]);
 // console.log(errors);
 
+// // get an element by ID 
+// const title = document.getElementById('page-title');
+// console.log(title);
 
+// // get elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+// console.log(errors[0]);
+
+// get elements by their tag name
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
