e88152bea9d7d4c188e4fe35b18d37c567ccf73d
Harmanpreet Singh <harmanpreetsingh4@academic.rrc.ca>
2024-05-08 09:52:55 -0500

    Using 'this' method - Object Literals

--- sandbox.js
+++ sandbox.js
@@ -493,3 +493,51 @@
 // console.log(user['name']);
 
 // console.log(typeof user);
+
+// let user = {
+//     name: 'crystal',
+//     age: 30,
+//     email: 'hrmn@hotmail.com',
+//     location: 'winnipeg',
+//     blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+//     login: function(){
+//         console.log('the user logged in');
+//     },
+//     logout: function(){
+//         console.log('the user logged out');
+//     }
+// };
+
+// user.login();
+// user.logout();
+
+let user = {
+    name: 'crystal',
+    age: 30,
+    email: 'hrmn@hotmail.com',
+    location: 'winnipeg',
+    blogs: ['why mac & cheese rules', '10 things to make with marmite'],
+    login(){
+        console.log('the user logged in');
+    },
+    logout(){
+        console.log('the user logged out');
+    },
+    logBlogs(){
+        // console.log(this.blogs);
+        console.log('this user has written the following blogs:');
+        this.blogs.forEach(blog =>{
+            console.log(blog);
+        })
+    }
+};
+
+user.logBlogs();
+console.log(this);
+
+
+
+
+
+
+
