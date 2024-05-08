9d37809a8e52fe77f49c45693b7a5a3e4e8c4755
Harmanpreet Singh <harmanpreetsingh4@academic.rrc.ca>
2024-05-08 13:30:25 -0500

    Query Selector and Query Selector All

--- sandbox.js
+++ sandbox.js
@@ -585,9 +585,14 @@
 // userOne.name = 'hrmn';
 // console.log(userOne, userTwo);
 
+// const para = document.querySelector('div.error');
 
+// console.log(para);
 
+// const paras = document.querySelectorAll('p');
+// const errors = document.querySelectorAll('.error');
 
-
+// console.log(paras[2]);
+// console.log(errors);
 
 
