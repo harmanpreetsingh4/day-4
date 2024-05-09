304bd872d9cc9d8da1bf75e845386f6fa45e25f1
Harmanpreet Singh <harmanpreetsingh4@academic.rrc.ca>
2024-05-09 08:46:05 -0500

    DOM - 'worked on innerHTML'

--- sandbox.js
+++ sandbox.js
@@ -608,3 +608,26 @@
 // const paras = document.getElementsByTagName('p');
 // console.log(paras);
 // console.log(paras[1]);
+
+const para = document.querySelector('p');
+
+// console.log(para.innerText);
+// para.innerText = "ninjas are awesome";
+
+const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+// });
+
+const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2>this is a new h2</h2>';
+
+const people = ['mario', 'luigi', 'yoshi'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+});
